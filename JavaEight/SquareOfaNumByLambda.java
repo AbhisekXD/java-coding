7e739a2b5d208803a4cc211669f73bb34ce6ebb6
AbhisekXD <abhiseknayak84@gmail.com>
2025-06-07 13:01:49 +0530

    nmnm

--- JavaEight/SquareOfaNumByLambda.java
+++ JavaEight/SquareOfaNumByLambda.java
@@ -2,7 +2,7 @@ import java.util.function.Function;
 
 public class SquareOfaNumByLambda {
     public static void main(String[] args) {
-        Function<Integer,Integer> num = n->n*n;
-        System.out.println(num.apply(8));
+        Function<Integer, Integer> square = n -> n * n;
+        System.out.println(square.apply(9));
     }
 }
